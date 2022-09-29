b7961265507c7bac658788ad7b279b4a6afa9a90
MichalLopatka <michalblopatka@gmail.com>
2022-09-29 13:12:13 +0200

    organised into methos

--- main.py
+++ main.py
@@ -1,43 +1,46 @@
+from typing import Dict
 
 
-def forward_alg():
-    X = 2
-    domino = r"||//||\||/\|"
-    domino = list(domino)
-    for _ in range(X):
-        changes = {"/": [], "\\": []}
-        for i in range(len(domino)-1):
-            if domino[i] == "/" and domino[i+1] == "|":
-                changes["/"].append(i+1)
-            if domino[i] == "|" and domino[i+1] == "\\":
+class Domino:
+    def domino_alg(self, iterations: int, domino: str, reverse: bool = False):
+        self.domino = list(domino)
+        if reverse:
+            for _ in range(iterations):
+                self.change_domino(changes=self.mark_reverse_changes())
+        else:
+            for _ in range(iterations):
+                self.change_domino(changes=self.mark_forward_changes())
+        self.print_domino()
+
+    def mark_forward_changes(self) -> Dict:
+        changes: Dict = {"/": [], "\\": []}
+        for i in range(len(self.domino) - 1):
+            if self.domino[i] == "/" and self.domino[i + 1] == "|":
+                changes["/"].append(i + 1)
+            if self.domino[i] == "|" and self.domino[i + 1] == "\\":
                 changes["\\"].append(i)
-        print(changes)
-        for el in changes["/"]:
-            domino[el] = "/"
-        for el in changes["\\"]:
-            domino[el] = "\\"
-        print(''.join(domino))
+        return changes
 
-def reverse_alg():
-    X = 2
-    domino = r"||////\\\|////|"
-    domino = list(domino)
-    for _ in range(X):
-        changes = {"|": []}
-        for i in range(len(domino)-1):
-            if domino[i] == "/" and domino[i+1] == "\\":
-                changes["|"].append(i+1)
+    def mark_reverse_changes(self) -> Dict:
+        changes: Dict = {"|": []}
+        for i in range(len(self.domino) - 1):
+            if self.domino[i] == "/" and self.domino[i + 1] == "\\":
+                changes["|"].extend([i, i + 1])
+            if self.domino[i] == "/" and self.domino[i + 1] == "|":
                 changes["|"].append(i)
-            if domino[i] == "/" and domino[i+1] == "|":
-                changes["|"].append(i)
-            if domino[i] == "|" and domino[i+1] == "\\":
-                changes["|"].append(i+1)
-        for el in changes["|"]:
-            domino[el] = "|"
-        print(''.join(domino))
+            if self.domino[i] == "|" and self.domino[i + 1] == "\\":
+                changes["|"].append(i + 1)
+        return changes
 
+    def change_domino(self, changes):
+        for key in changes:
+            for el in changes[key]:
+                self.domino[el] = str(key)
 
+    def print_domino(self):
+        print("".join(self.domino))
 
 if __name__ == "__main__":
-    # forward_alg()
-    reverse_alg()
+    domino = Domino()
+    domino.domino_alg(iterations=1, domino=r"||//||\||/\|")
+    domino.domino_alg(iterations=2, domino=r"||////\\\|////|", reverse=True)
